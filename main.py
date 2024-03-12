c17d408afb706227bc02331084cd88874ae86ce4
zahar658 <zaharmormil@gmail.com>
2024-03-12 17:35:51 +0300

    12.03.2024 17:35

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
 import pandas
+class Person:
+    name = -1
+    lastname = -1
+    surname = -1
 
-# excel_data_df = pandas.read_excel('records.xlsx', sheet_name='Employees')
-#
-# # print whole sheet data
-# print(excel_data_df)
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
-    excel_data_df = pandas.read_excel('lol.xlsx', sheet_name='Лист1')
+    excel_data_df = pandas.read_excel('Общая таблица НФИ плюс БИ.xlsx', sheet_name='Лист1')
+
+    a = excel_data_df['ФИО клиента']
+    print(a)
+    pop = []
+    for p in a:
+        hery = Person()
+        hery.name = p
+        pop.append(hery)
+        print(p)
+    print('\n')
+    print(pop[5].name)
+
 
     # print whole sheet data
-    print(excel_data_df)
-    print("\n")
-    print(excel_data_df.columns.name)
-    print_hi('PyCharm')
+    # print(excel_data_df)
+    # print("\n")
+    # print(excel_data_df.columns.name)
+    # print_hi('PyCharm')
 
